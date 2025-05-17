d13b2c9fac6856d62b764f80cc8720cb0a8fc00b
sultan19981998 <sultan.saif.alaamri@gmail.com>
2025-05-17 13:27:12 +0400

    Initial commit - Django job project

--- job/models.py
+++ job/models.py
@@ -16,7 +16,16 @@ class Job(models.Model):
     vacancy=models.IntegerField(default=1)
     salary=models.IntegerField(default=0)
     experience=models.IntegerField(default=1)
-
+    category= models.ForeignKey('Category',on_delete=models.CASCADE ,null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
